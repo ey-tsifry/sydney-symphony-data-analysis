802451fa3def7a8d00858584907184364eb05dbc
ey-tsifry <55221757+ey-tsifry@users.noreply.github.com>
2022-05-21 22:16:52 +1000

    Convert DBRecord to pydantic

--- data_models/sqlite_db.py
+++ data_models/sqlite_db.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Note: DBRecord is currently a data class instead of a SQLAlchemy model because creating a full
-SQLAlchemy model and its related scaffolding seemed like over-engineering for this project's
-particular use case.
-
-(Future migration to SQLAlchemy models is always possible if the use case becomes more complex).
+SQLite DB record model
 """
-
-from dataclasses import dataclass
-from typing import Dict
+from pydantic import BaseModel
 
 
-@dataclass
-class DBRecord:
+class DBRecord(BaseModel):
     """
-    Data class to represent a SSO HTML record in the SQLite DB.
+    Class to represent a SSO HTML record in the SQLite DB.
 
     html_content is represented as a string so that it doesn't crash Pandas.
     """
@@ -23,9 +16,3 @@ class DBRecord:
     year: int
     key: str
     html_content: str
-
-    @classmethod
-    def fields(cls):
-        """Return dictionary of data class fields."""
-        field_dict: Dict = {field_name: field_name for field_name in cls.__dataclass_fields__}
-        return field_dict
