1dded6f01f935920509a3c49f1f6194d89cfce6e
ey-tsifry <55221757+ey-tsifry@users.noreply.github.com>
2022-05-24 21:08:16 +1000

    Add common JSON calendar input year validator

--- sso_utilities/common.py
+++ sso_utilities/common.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Common constants and utility functions."""
 
 from typing import List
+
 
 # earliest and latest years for which is there are full SSO season data sets
 EARLIEST_SSO_SEASON_YEAR: int = 2018
 LATEST_SSO_SEASON_YEAR: int = 2022
+
+# year when SSO season calendar data switched to JSON
+JSON_CALENDAR_START_YEAR: int = 2021
 
 # default SQLite DB filename (may or may not already exist)
 DEFAULT_SQLITE_DB: str = f"sso_html_{EARLIEST_SSO_SEASON_YEAR}_{LATEST_SSO_SEASON_YEAR}.db"
@@ -41,4 +46,19 @@ def validate_sso_input_years(input_years: List[int]) -> None:
         raise ValueError(f"Input years cannot be earlier than {EARLIEST_SSO_SEASON_YEAR}")
     if max_year > LATEST_SSO_SEASON_YEAR:
         raise ValueError(f"Input years cannot be later than {LATEST_SSO_SEASON_YEAR}")
+    return None
+
+
+def validate_sso_json_calendar_year(
+    season_year: int, latest_year: int = LATEST_SSO_SEASON_YEAR
+) -> None:
+    """
+    Validate that JSON calendar input year is >= 2021 and <= the latest SSO season.
+
+    :param season_year: Input season year
+    :param latest_year: Latest SSO season (year)
+    :return: None if input year is within a valid range
+    """
+    if season_year < JSON_CALENDAR_START_YEAR or season_year > latest_year:
+        raise ValueError(f"Input JSON calendar year must be between 2021 and {latest_year}")
     return None
